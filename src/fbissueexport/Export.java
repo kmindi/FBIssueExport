74939e416be0b17c72fffc6f6e87b44f9e149e37
Kai Mindermann <kai.mindi@gmx.de>
2014-11-12 21:41:22 +0100

    adds bitbucket.org untested export code
    adds some documentation comments

--- src/fbissueexport/Export.java
+++ src/fbissueexport/Export.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -17,10 +18,13 @@ import org.apache.http.HeaderElement;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.ParseException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IProject;
@@ -79,9 +83,98 @@ public class Export {
 		}
 	}
 	
+	
+	/**
+	 * Calls the appropriate method for the corresponding platform
+	 * @param platformURLPart
+	 * @param owner
+	 * @param repoName
+	 * @return
+	 */
 	protected boolean exportToPlatformTracker(String platformURLPart, String owner, String repoName) {
+		platformURLPart = platformURLPart.toLowerCase();
 		
-		String apiRepoUrl = "https://api.github.com/repos/";
+		switch(platformURLPart) {
+		case "github.com": return exportToGitHubTracker(owner, repoName);
+		case "bitbucket.org": return exportToBitbucketTracker(owner, repoName);
+		default: return false;
+		}
+	}
+	
+	/**
+	 * Trys to export to Bitbucket
+	 * 
+	 * @todo untested!!
+	 * 
+	 * @param owner
+	 * @param repoName
+	 * @return
+	 */
+	protected boolean exportToBitbucketTracker(String owner, String repoName) {
+		
+		final String apiRepoUrl = "https://api.bitbucket.org/2.0/repositories/";
+		String issueRepo = owner + "/" + repoName;
+		
+		try {
+			ResponseWithEntity response = httpGetRequest(apiRepoUrl + issueRepo);
+			if(response == null) {
+				logger.warn("no response");
+				return false;
+			}
+			
+			// for Bitbucket
+			// check per API if the project is a fork 
+			// check for parent and if parent exists for full_name
+			logger.debug("checking if this repo (" + issueRepo + ") is a fork?");
+			ObjectMapper mapper = new ObjectMapper();
+			Map<String, Object> repoData = mapper.readValue(response.getEntity(), Map.class);
+			Object parentData = repoData.get("parent");
+			
+			if(parentData != null) {
+				Map<String,String> parentD = (Map<String, String>) parentData;
+				issueRepo = parentD.get("full_name");
+				logger.debug("is forked from: " + issueRepo);
+			}
+			
+			// TODO use isBugAlreadyReported to check if the bug was already reported
+			
+			// if the bug is not filed yet create a new issue
+			// https://github.com/<OWNER>/<REPOSITORY>/issues/new?title=<TITLE>&body=<DESCRIPTION>
+			String title = bug.getMessageWithoutPrefix();
+			String description = bug.getBugPattern().getDetailText(); // TODO include all relevant information and format better
+			
+			// TODO provide GUI to edit the issue before reporting
+			
+			HttpPost request = new HttpPost("https://bitbucket.org/api/1.0/repositories/" + issueRepo + "/issues");
+			
+			List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+			params.add(new BasicNameValuePair("title", title));
+			params.add(new BasicNameValuePair("content", description));
+			request.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+			
+			logger.debug("request line:" + request.getRequestLine());
+			CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+            HttpResponse result = httpClient.execute(request);
+            
+            // TODO open issue in webbrowser
+            
+			return true;
+			
+		} catch (ParseException | IOException e) {
+			logger.error(e.getMessage() + "\n" + e.getStackTrace());
+		};
+		
+		return false;
+	}
+	
+	/**
+	 * Creates a new issue for the bug on GitHub
+	 * @param owner
+	 * @param repoName
+	 * @return
+	 */
+	private boolean exportToGitHubTracker(String owner, String repoName) {
+		final String apiRepoUrl = "https://api.github.com/repos/";
 		String issueRepo = owner + "/" + repoName;
 		
 		try {
@@ -116,6 +209,7 @@ public class Export {
 			uriBuilder.addParameter("title", title);
 			uriBuilder.addParameter("body", description);
 			openWebPage(uriBuilder.build());
+			return true;
 			
 		} catch (ParseException | IOException | URISyntaxException e) {
 			logger.error(e.getMessage() + "\n" + e.getStackTrace());
@@ -123,7 +217,11 @@ public class Export {
 		
 		return false;
 	}
-	
+
+	/**
+	 * Opens a new browser window or new tab if already open
+	 * @param uri
+	 */
 	public static void openWebPage(URI uri) {
 	    Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
 	    if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
@@ -147,14 +245,14 @@ public class Export {
 	 */
 	@SuppressWarnings("unused")
 	private Boolean isBugAlreadyReported(BugInstance bug) {
-		// search issue liste for Bug-ID (bug.getInstanceHash()) and dont create a new rather redirect to the existing issue
+		// search issue list for Bug-ID (bug.getInstanceHash()) and don't create a new rather redirect to the existing issue
 		// https://api.github.com/repos/<OWNER>/<REPOSITORY>/issues
 		
 		String apiRepoUrl = "https://api.github.com/repos/";
 		String issueRepo = "kmindi/yii2";
 		
 		ResponseWithEntity response = httpGetRequest(apiRepoUrl + issueRepo + "/issues");
-		// loop throug paginated response until we find a bug with our ID or reach the end
+		// loop through paginated response until we find a bug with our ID or reach the end
 		// care about paginated answers using https://developer.github.com/guides/traversing-with-pagination/ to navigate 
 		// always use the provided link header
 		// link header uses "<" and ">" to enclose the url
@@ -178,6 +276,11 @@ public class Export {
 		return false;
 	}
 	
+	/**
+	 * Performs a HTTP GET Request.
+	 * @param url
+	 * @return ResponseWithEntity(HTTPResponse, String entity)
+	 */
 	private ResponseWithEntity httpGetRequest(String url) {
 		logger.info("requesting url:"+url);
 		try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
